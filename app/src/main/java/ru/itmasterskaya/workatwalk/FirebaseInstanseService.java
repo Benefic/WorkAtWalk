540578e89048e7a24fa8504ca5390fa9b8ed2837
benefic <abenefic@gmail.com>
2017-01-19 14:22:24 +0300

    Firebase

---
+++ app/src/main/java/ru/itmasterskaya/workatwalk/FirebaseInstanseService.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright abenefic (c) 2017.
+ */
+
+package ru.itmasterskaya.workatwalk;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by benefic on 19.01.17.
+ */
+
+public class FirebaseInstanseService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
